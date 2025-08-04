9dd9cae18d6d5cc9464c95ecfdcb35efedba026f
JhonJerez <jhonx3099@gmail.com>
2025-08-04 10:43:48 -0500

    Agregar clasificación de IMC según rangos de peso

--- IMC.py
+++ IMC.py
@@ -7,4 +7,13 @@ imc = weight/height**2
 
 print(f"{name} tu IMC es {imc:.2f}")
 
-|
+if imc < 18.5:
+    print("Peso isuficiente")
+elif imc <= 24.9:
+    print("Peso saludable")
+elif imc<= 34.9:
+    print("Obesidad clase I")
+elif imc<= 39.9:
+    print("Obesidad clase II")
+else:
+    print("Obesidad clase III")
